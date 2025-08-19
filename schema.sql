cbb1654205c9242a31b5a982ca7d9fbbd6103638
haikoj@users.noreply.github.com <you@example.com>
2025-08-19 14:34:08 +0300

    Fix review_id type to INTEGER

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE users (
     id INTEGER PRIMARY KEY,
     username TEXT UNIQUE,
     password_hash TEXT
-)
+);
 
 CREATE TABLE reviews (
     id INTEGER PRIMARY KEY,
@@ -12,7 +12,7 @@ CREATE TABLE reviews (
     grade INTEGER,
     user_id INTEGER REFERENCES users,
     review_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
+);
 
 CREATE TABLE comments (
     id INTEGER PRIMARY KEY,
@@ -20,17 +20,17 @@ CREATE TABLE comments (
     user_id INTEGER REFERENCES users,
     comment TEXT NOT NULL,
     comment_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
+);
 
 CREATE TABLE classes (
     id INTEGER PRIMARY KEY,
     title TEXT,
     value TEXT
-)
+);
 
 CREATE TABLE review_classes (
     id INTEGER PRIMARY KEY,
-    review_id TEXT REFERENCES reviews,
+    review_id INTEGER REFERENCES reviews,
     title TEXT,
     value TEXT
-)
+);
